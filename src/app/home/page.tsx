c30409eabc48030e4c7d2910ef80dd83bd6547c7
Dwi Gunardi Meinaki <dwigunardi@gmail.com>
2024-03-13 17:02:13 +0700

    add: search fucntion and render

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -34,46 +34,63 @@ export default async function Home({
   const tvSeries: MovieApi = await GetSeriesPopular({
     limit: 10,
     page: currentPageSeries || 1,
-  })
- 
+  });
+
   return (
     <main>
       <SliderCarousel
         dataMovie={movieNow?.results}
         dataGenre={genreListMovie?.genres}
       />
-      <PageTitle title="Latest Movie" />
-      <div className="mt-10">
-        <div className="container">
-          <Suspense fallback={<LoadingRoute height={"400px"} width={"full"} />}>
-            <SliderCard
-              movies={movieData?.results}
-              dataGenre={genreListMovie?.genres}
-              totalPage={movieData?.total_pages > 499 ? 499 : movieData?.total_pages}
-              query={query}
-              totalResults={movieData?.total_results}
-              currentPage={currentPage}
-              typePage="movie"
-            />
-          </Suspense>
+      {query ? (
+        <div className="mt-10 container">
+          <PageTitle title={`Search: ${query}`} />
+
         </div>
-      </div>
-      <PageTitle title="Latest TV Series" />
-      <div className="mt-10">
-        <div className="container">
-          <Suspense fallback={<LoadingRoute height={"400px"} width={"full"} />}>
-            <SliderCard
-              movies={tvSeries?.results}
-              dataGenre={genreListTv?.genres}
-              totalPage={tvSeries?.total_pages > 499 ? 499 : tvSeries?.total_pages}
-              query={query}
-              totalResults={tvSeries?.total_results}
-              currentPage={currentPageSeries}
-              typePage="series"
-            />
-          </Suspense>
+      ) : (
+        <div>
+          <PageTitle title="Latest Movie" />
+          <div className="mt-10">
+            <div className="container">
+              <Suspense
+                fallback={<LoadingRoute height={"400px"} width={"full"} />}
+              >
+                <SliderCard
+                  movies={movieData?.results}
+                  dataGenre={genreListMovie?.genres}
+                  totalPage={
+                    movieData?.total_pages > 499 ? 499 : movieData?.total_pages
+                  }
+                  query={query}
+                  totalResults={movieData?.total_results}
+                  currentPage={currentPage}
+                  typePage="movie"
+                />
+              </Suspense>
+            </div>
+          </div>
+          <PageTitle title="Latest TV Series" />
+          <div className="mt-10">
+            <div className="container">
+              <Suspense
+                fallback={<LoadingRoute height={"400px"} width={"full"} />}
+              >
+                <SliderCard
+                  movies={tvSeries?.results}
+                  dataGenre={genreListTv?.genres}
+                  totalPage={
+                    tvSeries?.total_pages > 499 ? 499 : tvSeries?.total_pages
+                  }
+                  query={query}
+                  totalResults={tvSeries?.total_results}
+                  currentPage={currentPageSeries}
+                  typePage="series"
+                />
+              </Suspense>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </main>
   );
 }
---
+++ src/repository/searchMovie.ts
@@ -0,0 +1,32 @@
+import { appConfig } from "@/config/appConfig";
+
+export async function SearchMovie({
+    query,
+    page,
+  }: {
+    query: string;
+    page: number | string;
+  }) {
+    try {
+      const response = await fetch(
+        `${appConfig.apiUrl}/search/movie?query=${query}&include_adult=false&language=en-US&page=${page}`,
+        {
+          next: { tags: ["searchMovie"] },
+          cache: "force-cache",
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            'accept': 'application/json',
+            'Authorization': `Bearer ${appConfig.appAccessToken}`
+          },
+        }
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return {
+        message: "something went wrong",
+        error,
+      };
+    }
+  }
